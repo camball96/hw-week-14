bcf889320f131f87297ad7956c81fbb97e6b9e70
cam.ball96 <cam.ball96@hotmail.com>
2021-07-26 20:49:09 +1000

    Started working on views and getting handlebars sorted

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const fs = require("fs");
+const handlebars = require("express-handlebars");
+const helpers = require("./utils/helpers");
 
 const app = express();
+
 
 const PORT = process.env.PORT || 3001;
 
 
+const hbs = handlebars.create({helpers})
+
+// establishing template engine - handlebars
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+
+
 app.get("/", (req, res) => {
-    res.sendFile('./views/index.html', { root:__dirname });
+    res.render('main');
+});
+app.get("/login", (req, res) => {
+    res.render('login');
+});
+app.get("/signup", (req, res) => {
+    res.render('signup');
 });
 
 
